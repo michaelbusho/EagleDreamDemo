16933d29dbb1a1cd9133bd8daf3bd56d0a2b8410
Mike <bushomichael@gmail.com>
2019-05-04 02:38:49 -0400

    get access token necessery for api requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const hbs = require('hbs');
+const request = require('request');
 
 //Setting important variables
 app.use(bodyParser.json());
@@ -36,9 +37,34 @@ app.post('/',(req, res) =>{
    var characterName= req.body.characterName;
    var realm = req.body.realm;
    console.log(`The character name is ${characterName} and the realm is ${realm}`);
+   callBlizzardApi(characterName, realm);
    res.end(JSON.stringify(character));
 });
 
+function callBlizzardApi(characterName, realm){
+	getAccessToken(function(token){
+		console.log(`String to request json data is:`);
+		var characterInfo = `https://us.api.blizzard.com/wow/character/${realm}/${characterName}?fields=stats&locale=en_US&access_token=${token}`;
+		console.log(characterInfo);
+	});
+}
+
+function getAccessToken(callback){ 
+	request.post({ 
+		url: 'https://us.battle.net/oauth/token',
+		headers: {
+			'content-type' : 'application/x-www-form-urlencoded', 
+			'Cache-Control' : 'no-cache' 
+		},
+		form: { grant_type: 'client_credentials' },   
+		auth: {
+			'user': '83ffd38037c040e694d5aaf72aee9a83',
+		    'pass': 'oAWUsgbakOvTq1Q0gp9xU9EPWivify0s'
+		}
+	},function(error, response, body){
+   		callback(JSON.parse(body).access_token);
+	}); 	
+}
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
