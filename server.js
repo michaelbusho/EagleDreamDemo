51bf03e83dff5722394aaee6d7b5065d3f1a0d46
Mike <bushomichael@gmail.com>
2019-05-03 21:48:15 -0400

    adding html template support

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const hbs = require('hbs');
 
 const character = {
 }
 
 //Middlewere
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+	res.render('index.hbs');
+});
+
+
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
