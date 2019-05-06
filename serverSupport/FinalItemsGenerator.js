92d634e8fc3f13abba94c214cea9f1b2cb7ad544
Mike <bushomichael@gmail.com>
2019-05-05 22:05:56 -0400

    bonus implementation- refactoring and unequiped body part bug fixed

--- serverSupport/FinalItemsGenerator.js
+++ serverSupport/FinalItemsGenerator.js
@@ -1,42 +1,19 @@
 
 function createFinalItems(itemsFullDesc, eqItems){
 	var finalItems ={}
-	itemsFullDesc.forEach(function(element) {
-  					if(element.id === eqItems.data.items.back.id){
-  						finalItems.back = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					} else if(element.id === eqItems.data.items.chest.id){
-  						finalItems.chest = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					} else if(element.id === eqItems.data.items.feet.id) {
-  						finalItems.feet = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					} else if(element.id === eqItems.data.items.finger1.id){
-  						finalItems.finger1 = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.finger2.id){
-  						finalItems.finger2 = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.hands.id){
-  						finalItems.hands = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.head.id){
-  						finalItems.head = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.legs.id){
-  						finalItems.legs = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.mainHand.id){
-  						finalItems.mainHand = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.neck.id){
-  						finalItems.neck = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-  					}else if(element.id === eqItems.data.items.shirt.id){
-              finalItems.shirt = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-            }else if(element.id === eqItems.data.items.shoulder.id){
-              finalItems.shoulder = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-            }else if(element.id === eqItems.data.items.trinket1.id){
-              finalItems.trinket1 = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-            }else if(element.id === eqItems.data.items.trinket2.id){
-              finalItems.trinket2 = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-            }else if(element.id === eqItems.data.items.waist.id){
-              finalItems.waist = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-            }else if(element.id === eqItems.data.items.wrist.id){
-              finalItems.wrist = itemPatern(element.id, element.name, element.buyPrice,element.sellPrice,element.requiredLevel);
-            }
-				});
-
+    for (var fullItemDescr in itemsFullDesc) {
+      for(var bodypart in eqItems){
+        if(itemsFullDesc[fullItemDescr].id === eqItems[bodypart].id){
+          finalItems[bodypart]  = itemPatern(
+            itemsFullDesc[fullItemDescr].id, 
+            itemsFullDesc[fullItemDescr].name, 
+            itemsFullDesc[fullItemDescr].buyPrice, 
+            itemsFullDesc[fullItemDescr].sellPrice, 
+            itemsFullDesc[fullItemDescr].requiredLevel
+            );
+        }
+      }
+    }
 	return finalItems;
 }
 
--- serverSupport/HandleBlizzardApi.js
+++ serverSupport/HandleBlizzardApi.js
@@ -1,10 +1,10 @@
 const token = require('./AcquireToken');
 const itemPatern = require('./FinalItemsGenerator');
+const itemsDescription = require('./getItemDescriptionsFull');
 const axios = require('axios');
 var characterUrl;
 var eqItemsUrl;
 var myToken;
-var itemExtra ={};
 
 function getBlizzardInfo(characterName, realm, callback){
 	token.getAccessToken(function(token){ 
@@ -15,20 +15,16 @@ function getBlizzardInfo(characterName, realm, callback){
 		.then(axios.spread(function (stats, eqItems){
 			var statistics = createStatsObject(stats);
 			var eqItemsObj = eqItems.data.items;
-			console.log(eqItemsObj);
-			var itemsID = createItemArrayID(eqItems.data.items);
+			var itemsID = getEquipedItemsID(eqItemsObj);
 
-			aquireItemData(itemsID, myToken,function(itemsFullDesc){
-			var finalItems ={};
+			itemsDescription.getBlizzardItemDescription(itemsID, myToken,function(itemsFullDesc){
+				var finalItems ={};
+				finalItems = itemPatern.createFinalItems(itemsFullDesc, eqItemsObj);
+				var statusMessage = {status: 'ok', reason: 'ok'};
+				var info = {statistics, statusMessage, finalItems};
 
-			finalItems = itemPatern.createFinalItems(itemsFullDesc, eqItems);
-			console.log(finalItems);
-			var statusMessage = {status: 'ok', reason: 'ok'};
-			var info = {statistics, eqItemsObj, statusMessage, finalItems};
-
-			callback(info);
+				callback(info);
 			});
-
 		})).catch(function (error) {
 			var statusMessage = error.response.data;
 			var stats = {};
@@ -39,36 +35,12 @@ function getBlizzardInfo(characterName, realm, callback){
 	});
 };
 
-
-
-
 function getCharacterStats(characterUrl) {
   return axios.get(characterUrl);
 }
 
 function getEquipedItems(eqItemsUrl) {
   return axios.get(eqItemsUrl);
-}
-
-function aquireItemData(itemsID, token,callback){
-	var promises = [];
-	var itemsProcessed =0;
-	var itemsHad = [];
-	itemsID.forEach(function(singleItemID){
-		url = `https://us.api.blizzard.com/wow/item/${singleItemID}?locale=en_US&access_token=${token}`
-		console.log(`to url einai : ${url}`);
-		promises.push(axios.get(url))
-	});
-
-	axios.all(promises).then(function(results) {
-		results.forEach(function(response) {
-			itemsProcessed++;
-			itemsHad.push(response.data);
-			if(itemsProcessed === itemsID.length){
-				callback(itemsHad);
-			}
-		});
-	});
 }
 
 function createStatsObject(stats){
@@ -93,112 +65,18 @@ function createStatsObject(stats){
 			};
 }
 
-function createItemArrayID(eqItems){
-	return itemsID =[
-		eqItems.back.id,
-		eqItems.chest.id,
-		eqItems.feet.id,
-		eqItems.finger1.id,	
-		eqItems.finger2.id,
-		eqItems.hands.id,
-		eqItems.head.id,
-		eqItems.legs.id,
-		eqItems.mainHand.id,
-		eqItems.neck.id,
-		eqItems.shirt.id,
-		eqItems.shoulder.id,
-		eqItems.trinket1.id,
-		eqItems.trinket2.id,
-		eqItems.waist.id,
-		eqItems.wrist.id			
-	];
-}
-function createFinalItems(itemsFullDesc, eqItems){
-	var finalItems ={}
-	itemsFullDesc.forEach(function(element) {
-  					if(element.id === eqItems.data.items.back.id){
-  						finalItems.back ={
-  							id: eqItems.data.items.back.id, 
-  							name: eqItems.data.items.back.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					} else if(element.id === eqItems.data.items.chest.id){
-  						finalItems.chest ={
-  							id: eqItems.data.items.chest.id, 
-  							name: eqItems.data.items.chest.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					} else if(element.id === eqItems.data.items.feet.id) {
-  						finalItems.feet ={
-  							id: eqItems.data.items.feet.id, 
-  							name: eqItems.data.items.feet.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					} else if(element.id === eqItems.data.items.finger1.id){
-  						finalItems.finger1 ={
-  							id: eqItems.data.items.finger1.id, 
-  							name: eqItems.data.items.finger1.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}else if(element.id === eqItems.data.items.finger2.id){
-  						finalItems.finger2 ={
-  							id: eqItems.data.items.finger2.id, 
-  							name: eqItems.data.items.finger2.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}else if(element.id === eqItems.data.items.hands.id){
-  						finalItems.hands ={
-  							id: eqItems.data.items.hands.id, 
-  							name: eqItems.data.items.hands.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}else if(element.id === eqItems.data.items.head.id){
-  						finalItems.head ={
-  							id: eqItems.data.items.head.id, 
-  							name: eqItems.data.items.head.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}else if(element.id === eqItems.data.items.legs.id){
-  						finalItems.legs ={
-  							id: eqItems.data.items.legs.id, 
-  							name: eqItems.data.items.legs.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}else if(element.id === eqItems.data.items.mainHand.id){
-  						finalItems.mainHand ={
-  							id: eqItems.data.items.mainHand.id, 
-  							name: eqItems.data.items.mainHand.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}else if(element.id === eqItems.data.items.neck.id){
-  						finalItems.neck ={
-  							id: eqItems.data.items.neck.id, 
-  							name: eqItems.data.items.neck.name,
-  							buyPrice: element.buyPrice,
-  							sellPrice: element.sellPrice,
-  							requiredLevel: element.requiredLevel
-  						}
-  					}
-				});
+function getEquipedItemsID(eqItems){
+	var cntr=0;
+	var itemsID = [];
 
-	return finalItems;
+	//first 2 results are never body parts
+	for (var bodyPart in eqItems) {
+		cntr++;
+		if(cntr>2){
+			itemsID.push(eqItems[bodyPart].id);
+		}
+	}
+	return itemsID;
 }
+
 module.exports.getBlizzardInfo = getBlizzardInfo;
