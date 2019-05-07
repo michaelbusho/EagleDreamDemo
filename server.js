1b173bee77f0fb240e4d9c32b6c8d944f4c68868
Mike <bushomichael@gmail.com>
2019-05-07 12:19:35 -0400

    html clean up

--- server.js
+++ server.js
@@ -11,9 +11,29 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+var bodyParts = [
+	'head',
+	'chest',
+	'shoulder',
+	'legs',
+	'feet',
+	'trinket1',
+	'trinket2',
+	'back',
+	'mainHand',
+	'offHand',
+	'neck',
+	'waist',
+	'wrist',
+	'shirt',
+	'finger1',
+	'finger2'
+	];
+
+var comperableCharcs = ['mike.jpg', 'fuo.jpg','regex.jpg','tweeps.jpg','lockino.jpg','briefdruid.jpg'];
 //Routes
 app.get('/', (req, res)=>{
-	res.render('index.hbs');
+	res.render('index.hbs',{bodyParts: bodyParts});
 });
 app.post('/',(req, res) =>{
 	var characterName= req.body.characterName;
