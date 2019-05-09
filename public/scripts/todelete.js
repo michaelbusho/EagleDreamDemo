c5400c7df411c3850c01c4dc1d9d6f8f1d4c3d19
Mike <bushomichael@gmail.com>
2019-05-09 02:02:32 -0400

    final fixes

--- public/scripts/todelete.js
+++
@@ -1,9 +0,0 @@
-    //apply bonus
-    var oldValue;
-    var newValue;
-    finalItems[key].bonus.forEach(function(bonus){ 
-      oldValue = Number($(`.${bonus.stat}`).text());
-      newValue = oldValue + bonus.amount;
-      $(`.${bonus.stat}`).text(newValue);
-      $(`.${bonus.stat}`).addClass("bonus");
-    });
